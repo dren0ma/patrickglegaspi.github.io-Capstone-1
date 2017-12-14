55a56b0a8d23335bf8ee4cc741ff6c0c6a44f4a5
dren0ma <patrickglegaspi@gmail.com>
2017-12-14 23:40:00 +0800

    modified csp1

--- js/script.js
+++ js/script.js
@@ -64,24 +64,24 @@ function totalItems() {
 	var getpizza = document.getElementById("optionpizza");
 	price[0] = parseInt(getpizza.options[getpizza.selectedIndex].value);
 	pizzaindex = parseInt(getpizza.options[getpizza.selectedIndex].id);
-	document.getElementById("order-item0").innerHTML = "Pizza: " + pizza[pizzaindex];
+	document.getElementById("order-item0").innerHTML = "Pizza:  " + pizza[pizzaindex];
 	
 	var getpasta = document.getElementById("optionpasta");
 	price[1] = parseInt(getpasta.options[getpasta.selectedIndex].value);
 	pastaindex = parseInt(getpasta.options[getpasta.selectedIndex].id);
-	document.getElementById("order-item1").innerHTML = "Pasta: " + pasta[pastaindex];
+	document.getElementById("order-item1").innerHTML = "Pasta:  " + pasta[pastaindex];
 	
 	var getsalad = document.getElementById("optionsalad");
 	price[2] = parseInt(getsalad.options[getsalad.selectedIndex].value);
 	saladindex = parseInt(getsalad.options[getsalad.selectedIndex].id);
-	document.getElementById("order-item2").innerHTML = "Salad: " + salad[saladindex];
+	document.getElementById("order-item2").innerHTML = "Salad:  " + salad[saladindex];
 	
 	var getdrinks = document.getElementById("optiondrinks");
 	price[3] = parseInt(getdrinks.options[getdrinks.selectedIndex].value);
 	drinksindex = parseInt(getdrinks.options[getdrinks.selectedIndex].id);
-	document.getElementById("order-item3").innerHTML = "Drinks: " + drinks[drinksindex];
+	document.getElementById("order-item3").innerHTML = "Drinks:  " + drinks[drinksindex];
 
 	total = price[0] + price[1] + price[2] + price[3];
 
-	document.getElementById("order-total").innerHTML = "Total Price: " + total;
+	document.getElementById("order-total").innerHTML = "Total Price:  " + total;
 };
